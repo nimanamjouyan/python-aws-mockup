d33862c2a36f84928ef5a14c351243a5e627a2ef
Nima Namjouyan <nimanamjouyan@gmail.com>
2021-09-22 00:31:08 +0930

    🔨 Started on adding terraform scripts in the terraform folder. ➖ Removed boto3.

--- api.py
+++ api.py
@@ -7,9 +7,6 @@ from modules import create_instance
 
 app = FastAPI(docs_url="/")
 
-with open("VERSION", "r") as f:
-    VERSION = f.read()
-
 
 @app.get("/main")
 def main(
@@ -17,8 +14,3 @@ def main(
     key_names: List[str] = Query(None, title="List of private keys", description="Place one private key per field."),
 ):
     return {"instance_ids": create_instance(model, key_names)}
-
-
-@app.get("/version")
-def version():
-    return VERSION
