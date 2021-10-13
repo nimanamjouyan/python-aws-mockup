24ec7fe6c00cbf32e2e36e89b9c696af38ae9364
Nima Namjouyan <nimanamjouyan@gmail.com>
2021-10-13 16:51:22 +1030

    ➕ Added python terraform to the dependency. 📝 Add documentation to the fastapi endpoint.

--- api.py
+++ api.py
@@ -5,10 +5,22 @@ from fastapi import Depends, FastAPI, Query
 from model import Model
 from modules import create_instance
 
-app = FastAPI(docs_url="/")
+
+description = """
+FastApi meets Terraform 💪
+####
+Making terraform API available through FastAPI which allows for spinning up EC2 instances provided a JSON of relevant parameters.
+"""
+
+app = FastAPI(
+    title="AWS Monster",
+    description=description,
+    version="0.0.1",
+    docs_url="/"
+    )
 
 
-@app.get("/main")
+@app.get("/main", tags=["Main"])
 def main(
     model: Model = Depends(),
     key_names: List[str] = Query(None, title="List of private keys", description="Place one private key per field."),
