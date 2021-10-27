633f0e76f53be156c31cb2b3fa958569211372a3
Nima Namjouyan <nimanamjouyan@gmail.com>
2021-10-28 00:45:48 +1030

    ✨ Added new endpoint for destroying the resources from the API.

--- api.py
+++ api.py
@@ -19,13 +19,15 @@ app = FastAPI(
 
 tf = Terraform(working_dir="./terraform")
 tf.init()
+params = []
+paths = []
+instance_params_global = InstanceParams()
 
 
-@app.post("/main", tags=["Main"])
-async def main(
+@app.post("/create", tags=["create"])
+def create(
     instance_names: List[str] = Query(None),
     public_keys: List[UploadFile] = File(...),
-    secrets: Secrets = Depends(),
     instance_params: InstanceParams = Depends(),
 ):
     if len(instance_names) != len(public_keys):
@@ -34,24 +36,39 @@ async def main(
             detail="The length of instance names must be equal to the number of keys supplied!!",
         )
     # TODO: Use the paths variable to remove/unlink all the temp paths eventually.
-    (params, paths) = params_to_list(public_keys, instance_names, instance_params)
+    instance_params_global = instance_params
+    (params, paths) = params_to_list(
+        public_keys, instance_names, instance_params_global
+    )
 
     return_code, stdout, stderr = tf.apply(
+        input=False,
         refresh=False,
         skip_plan=True,
         capture_output=False,
         var={
-            "AWS_ACCESS_KEY": secrets.aws_access_key_id.get_secret_value(),
-            "AWS_SECRET_KEY": secrets.aws_secret_key_id.get_secret_value(),
             "AWS_REGION": instance_params.region_name,
             "INSTANCES": params,
         },
     )
     # TODO: See if the instance id or public ip can be returned rather than what is below. id and ip are more useful things!!
     return {"return_code": return_code, "stdout": stdout, "stderr": stderr}
-    # print(return_code, "\n", stdout, "\n", stderr)
-    # print(save_upload_file_tmp("/home/morph/mykey.pub"))
-    # data = json.load(file.file)
-    # print(data)
-    # print(save_upload_file_tmp(file))
-    # return {"instance_ids": create_instance(model, key_names)}
+
+
+@app.post("/destroy", tags=["destroy"])
+def destroy():
+
+    return_code, stdout, stderr = tf.destroy(
+        capture_output="yes",
+        no_color=IsNotFlagged,
+        force=IsNotFlagged,
+        auto_approve=True,
+        var={
+            "AWS_REGION": instance_params_global.region_name,
+            "INSTANCES": params,
+        },
+    )
+    for path in paths:
+        path.unlink()
+    # TODO: See if the instance id or public ip can be returned rather than what is below. id and ip are more useful things!!
+    return {"return_code": return_code, "stdout": stdout, "stderr": stderr}
